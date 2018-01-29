5986fbd7c6a30d80a3253bc56f065de9b87900ae
BDMayhem <brianjdiaz@gmail.com>
2018-01-29 16:55:36 -0500

    portfolio styling

--- index.js
+++ index.js
@@ -1,90 +1,85 @@
-const port = {
-  tribute: {
+const port = [
+  {
     name: "A Tribute to John Irving",
     link: "https://bdmayhem.github.io/irving/",
     image: "assets/tribute.png",
-  },
-   quote: {
+  },{
     name: "Movie Quote Machine",
     link: "https://bdmayhem.github.io/movie-quotes/",
     image: "assets/quote.png",
-  },
-  
-  weather: {
+  },{
     name: "Local Weather",
     link: "https://bdmayhem.github.io/weather/",
     image: "assets/weather.png",
-  },
-  wiki: {
+  },{
     name: "Search Wikipedia",
     link: "https://bdmayhem.github.io/search-wiki/",
     image: "assets/wiki.png",
-  },
-  twitch: {
+  },{
     name: "Twitch Monitor",
     link: "https://bdmayhem.github.io/twitch/",
     image: "assets/twitch.png",
-  },
-  calculator: {
-    name: "Javascript Calculator",
-    link: "https://bdmayhem.github.io/calculator/",
-    image: "assets/calculator.png",
-  },
-  pomodoro: {
+  // },{
+  //   name: "Javascript Calculator",
+  //   link: "https://bdmayhem.github.io/calculator/",
+  //   image: "assets/calculator.png",
+  },{
     name: "Pomodoro Clock",
     link: "https://bdmayhem.github.io/pomodoro/",
     image: "assets/pomodoro.png",
-  },
-  tictactoe: {
-    name: "Tic-Tac-Toe",
-    link: "https://bdmayhem.github.io/tictactoe/",
-    image: "assets/tic.png",
-  },
-  simon: {
+  // },{
+  //   name: "Tic-Tac-Toe",
+  //   link: "https://bdmayhem.github.io/tictactoe/",
+  //   image: "assets/tic.png",
+  },{
     name: "Simon Game",
     link: "https://bdmayhem.github.io/simon/",
     image: "assets/simon.png",
-  },
-  circles: {
+  },{
     name: "Random, Useless Circles",
     link: "https://bdmayhem.github.io/circles/",
     image: "assets/circles.jpg"
-  },
-  ga: {
+  },{
     name: "Green Apple Weddings",
     link: "http://greenappleweddings.com",
     image: "assets/ga.jpg"
-  },
-  dtc: {
+  },{
     name: "Deep Town Calc",
     link: "https://bdmayhem.github.io/DeepTownCalc",
     image: "assets/dtc.png",
     description: "Vanilla JS SPA to calculate best choices for mobile game 'Deep Town'"
-  },
-  grandmasRecipes: {
+  },{
     name: "Grandma's Recipes",
     link: "https://bdmayhem.github.io/GrandmasRecipes/",
     image: "assets/grandmasrecipes.png",
     description: "React-based SPA recipe book allowing users to create, read, update, and destroy searchable Local Storage data "
   }
-};
+];
 
-$.each(port, function(key, value){
-  let hasDescription = '';
+const portItems = document.querySelector('.port-items');
 
-  if(value.hasOwnProperty('description')) {
-     hasDescription = `<div class="ui fluid description">${value.description}</div>`;
-  };
-  
-  $(".stackable").prepend(`<div class="column"><figure><a href="${value.link}" target="_blank">${hasDescription}<img class="ui fluid image port-image" src="${value.image}" alt="${value.name}"<figcaption>${value.name}</figcaption></a></figure></div>`);
+port.reverse().forEach(element => {
+  const portItem = document.createElement('div');
+  portItem.classList.add('port-item');
 
+  const itemLink = document.createElement('a');
+  itemLink.href = element.link;
+  portItem.appendChild(itemLink);
+
+  const itemImage = document.createElement('img');
+  itemImage.src = element.image;
+  itemLink.appendChild(itemImage)
+
+  const itemName = document.createElement('h3');
+  itemName.appendChild(document.createTextNode(element.name));
+  itemLink.appendChild(itemName);
+
+  if (element.hasOwnProperty('description')) {
+    const itemDesc = document.createElement('p');
+    const desc = document.createTextNode(element.description);
+    itemDesc.appendChild(desc);
+    portItem.appendChild(itemDesc);
+  }
+
+  portItems.appendChild(portItem);
 });
-
-function getWidth() {
-  const imageWidth = $(".port-image").width();
-  $(".description").css("height", imageWidth)
-}
-
-setTimeout(() => getWidth(), 100);
-
-$(window).resize(getWidth);
